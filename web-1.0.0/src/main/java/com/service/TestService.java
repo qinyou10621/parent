c5b21fe8c86d0d616555d86e944e7cb46ee4371d
Administrator <787675165@qq.com>
2017-01-17 18:12:20 +0800

    ASDFASDFA

--- web-1.0.0/src/main/java/com/service/TestService.java
+++ web-1.0.0/src/main/java/com/service/TestService.java
@@ -1,9 +1,9 @@
 package com.service;
 
-import com.pojo.Classes;
-import com.pojo.Student;
+        import com.pojo.Classes;
+        import com.pojo.Student;
 
-import java.util.List;
+        import java.util.List;
 
 public interface TestService {
     public List<Classes> findInformation(String className);
@@ -11,4 +11,8 @@ public interface TestService {
     public void insertUser(String username, String password);
 
     public void update(Student student);
+
+    public void redisTestMS();
+
+    public void redisTestCluster();
 }
--- web-1.0.0/src/main/java/com/service/TestServiceImpl.java
+++ web-1.0.0/src/main/java/com/service/TestServiceImpl.java
@@ -5,16 +5,35 @@ import com.pojo.Classes;
 import com.pojo.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class TestServiceImpl implements TestService {
 
-
     @Autowired
     private Mappers mappers;
+
+    @Autowired
+    private ShardedJedisPool sharedJedisPool;
+
+    @Autowired
+    private JedisCluster aa;
+
+    /**
+     * 测试redis客户端读写redis缓存
+     */
+    public void redisTestMS() {
+        ShardedJedis shardeJedis = sharedJedisPool.getResource();
+        shardeJedis.set("key1", "abc");
+    }
+
+    public void redisTestCluster() {
+
+    }
 
     public List<Classes> findInformation(String className) {
 
